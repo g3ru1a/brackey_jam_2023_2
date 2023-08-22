a4465dce60dccd84bc575838c7f626d61132f630
G3ru1a <egidiufarcas@maze.ws>
2023-08-22 16:31:02 +0100

    Finished Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,15 @@
 using System.Collections;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public AudioSource audioSource;
-    public Transform startingPoint;
-    public Transform endingPoint;
+    public float trackBPM = 128;
+    [Range(0,1)]
+    public float trackOffset = 0.1f;
 
     private float _movementSpeed;
     private float _audioSpeed;
@@ -16,10 +17,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private LayerMask _groundLayer;
     private Rigidbody2D _rigidBody;
-    private Transform _groundCheck;
+    private CapsuleCollider2D _capsuleCollider;
     
-    private float jumpHeight = .7f;
-    private float _jumpPower;
+    private float _jumpHeight = 2f;
+    private float _jumpDuration;
     private float _fallMultiplier = 12f;
     private Vector2 _fallForce;
 
@@ -28,36 +29,35 @@ public class PlayerMovement : MonoBehaviour
     private float _jumpBuffer = 0.2f;
     private float _jumpBufferCounter = 0;
     private bool _isJumping = false;
+    private bool _isOnGround = false;
 
     private bool _gameStarted = false;
+
+    Vector2 startingPos = Vector2.zero;
+    Vector2 endingPos = Vector2.zero;
 
     void Awake()
     {
         _rigidBody = gameObject.GetComponent<Rigidbody2D>();
-        _groundCheck = gameObject.transform.Find("GroundCheck").gameObject.transform;
+        _capsuleCollider = gameObject.GetComponent<CapsuleCollider2D>();
+
+        float BPS = trackBPM / 60f;
+        _movementSpeed = (BPS * 4) - trackOffset;
+        _jumpDuration = 60f / trackBPM;
     }
 
     void Start()
     {
+        MoveOnGround();
         _gameStarted = true;
-        
-        float distance = endingPoint.position.x - startingPoint.position.x; // distance in units
-        float time = audioSource.clip.length; // time in seconds
-        _audioSpeed = distance/time;
 
-        _movementSpeed = 0f;
-        // Debug.Log(_movementSpeed);
-        _jumpPower = Mathf.Sqrt(jumpHeight * (Physics2D.gravity.y * _rigidBody.gravityScale) * -2) * _rigidBody.mass;
         _fallForce = Vector2.up * (Physics2D.gravity.y * (_fallMultiplier - 1));
     }
 
     void Update()
     {
-        
-        // Debug.Log(audioSource.time + " || " + audioSource.timeSamples);
-        // To be removed once tweaking is complete
-        _fallForce = Vector2.up * (Physics2D.gravity.y * (_fallMultiplier - 1));
-        // .\
+        _isOnGround = IsOnGround();
+
         if(Keyboard.current.spaceKey.wasPressedThisFrame ||
             Mouse.current.leftButton.wasPressedThisFrame)
         {
@@ -73,50 +73,140 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if(_coyoteTimeCounter > 0f && _jumpBufferCounter > 0f && !_isJumping){
-            Jump();
-            StartCoroutine(JumpCooldown());
+            StartCoroutine(Jump());
         }
 
         if(Keyboard.current.rKey.wasPressedThisFrame){
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
+        if(Keyboard.current.qKey.wasPressedThisFrame){
+            Application.Quit();
+        }
     }
 
     void FixedUpdate(){
-        // Multiply Fall Force
         if(_rigidBody.velocity.y < 0){
             _rigidBody.AddForce(_fallForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
         }
-        // _rigidBody.velocity = new Vector2(5, _rigidBody.velocity.y);
-        if(audioSource.time != 0){
-            gameObject.transform.position += _audioSpeed * Vector3.right * Time.deltaTime;
-        }else{
-            gameObject.transform.position += _movementSpeed * Vector3.right * Time.deltaTime;
+
+        if(!_isJumping){
+            gameObject.transform.position += _movementSpeed * Vector3.right * Time.fixedDeltaTime;
         }
     }
 
     bool IsOnGround()
     {
-        return Physics2D.OverlapCircle(_groundCheck.position, 0.1f, _groundLayer);;
+        Vector2 groundCheckPosition = new Vector2(gameObject.transform.position.x, _capsuleCollider.bounds.min.y);
+        return Physics2D.OverlapCircle(groundCheckPosition, 0.1f, _groundLayer);
     }
 
-    void Jump(){
-        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 0);
-        _rigidBody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
-    }
-
-    IEnumerator JumpCooldown()
-    {
+    IEnumerator Jump(){
         _isJumping = true;
-        yield return new WaitForSeconds(0.4f);
+
+        startingPos = new Vector2(gameObject.transform.position.x, _capsuleCollider.bounds.min.y);
+        // endingPos = startingPos + (Vector2.right * (_movementSpeed * _jumpDuration)) - new Vector2(0.2f, 0);
+        endingPos = startingPos + (Vector2.right * (_movementSpeed * _jumpDuration));
+
+        Vector2 raycastOrigin = endingPos + Vector2.up * _jumpHeight;
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.down, Mathf.Infinity, _groundLayer);
+
+        float minYLand = (startingPos.y + _jumpHeight) - hit.distance;
+        Vector2 adjustedEndingPos = new Vector2(endingPos.x, minYLand);
+
+        float timeLeft = _jumpDuration;
+        while(timeLeft >= 0f){
+            float normal = 1 - Mathf.InverseLerp(0, _jumpDuration, timeLeft);
+            float x = Mathf.Lerp(startingPos.x, endingPos.x, normal);
+            float y = (Mathf.Sin(Mathf.Lerp(0, Mathf.PI, normal)) * _jumpHeight) 
+                + Mathf.Lerp(startingPos.y, adjustedEndingPos.y, normal);
+
+            y += (_capsuleCollider.bounds.size.y / 2);
+            gameObject.transform.position = new Vector3(x,
+                y, gameObject.transform.position.z);
+
+            yield return new WaitForFixedUpdate();
+            timeLeft -= Time.fixedDeltaTime;
+        }
+        //Move to ground and take a step
+        MoveOnGround();
+        gameObject.transform.position += _movementSpeed * Vector3.right * Time.fixedDeltaTime;
         _isJumping = false;
+    }
+
+    void MoveOnGround(){
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, _capsuleCollider.bounds.min.y),
+            Vector2.down, Mathf.Infinity, _groundLayer);
+        float groundY = _capsuleCollider.bounds.min.y - hit.distance + (_capsuleCollider.bounds.size.y / 2);
+        transform.position = new Vector3(transform.position.x, groundY, transform.position.z);
     }
 
     void OnDrawGizmos()
     {
         if(_gameStarted){
-            Gizmos.DrawWireSphere(_groundCheck.position, 0.1f);
+            Vector2 groundCheckPosition = new Vector2(gameObject.transform.position.x, _capsuleCollider.bounds.min.y);
+            Gizmos.DrawWireSphere(groundCheckPosition, 0.1f);
         }
-        Gizmos.DrawLine(gameObject.transform.position, gameObject.transform.position + Vector3.right);
+
+        _capsuleCollider = gameObject.GetComponent<CapsuleCollider2D>();
+
+        float botY = _capsuleCollider.bounds.min.y;
+        Vector2 pos1 = new Vector2(gameObject.transform.position.x - _capsuleCollider.bounds.size.x / 2, botY);
+        Vector2 pos2 = new Vector2(gameObject.transform.position.x + _capsuleCollider.bounds.size.x / 2, botY);
+        Gizmos.DrawLine(pos1, pos2);
+
+        float BPS = trackBPM / 60f;
+        _movementSpeed = (BPS * 4) - trackOffset;
+        _jumpDuration = 60f / trackBPM;
+
+
+        // startingPos = new Vector2(gameObject.transform.position.x,
+        //     gameObject.transform.position.y - _capsuleCollider.bounds.size.y / 2);
+        // endingPos = startingPos +(Vector2.right * (_movementSpeed * _jumpDuration));
+        
+        //Flat sin arch
+        Gizmos.color = Color.red;
+        PlotSinCurveGizmo(startingPos, endingPos, _jumpDuration, Time.fixedDeltaTime);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(startingPos, endingPos);
+
+        //Adjusted for ground collision prediction
+        Vector2 raycastOrigin = endingPos + Vector2.up * _jumpHeight;
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.down, Mathf.Infinity, _groundLayer);
+        Gizmos.DrawLine(raycastOrigin, raycastOrigin + Vector2.down * hit.distance);
+
+        float minYLand = (startingPos.y + _jumpHeight) - hit.distance;
+        Vector2 adjustedEndingPos = new Vector2(endingPos.x, minYLand);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(startingPos, new Vector2(endingPos.x, minYLand));
+
+        Gizmos.color = Color.green;
+        PlotSinCurveGizmo(startingPos, adjustedEndingPos, _jumpDuration,
+            Time.fixedDeltaTime, 0, _capsuleCollider.bounds.size.y / 2);
+    }
+
+    void PlotSinCurveGizmo(Vector2 startingPos, Vector2 endingPos, float curveLength, float step, float xOffset = 0, float yOffset = 0)
+    {
+        Vector2 prev_pos = Vector2.zero;
+        float remaining = _jumpDuration;
+        while(remaining >= 0f){
+            float normal = 1 - Mathf.InverseLerp(0, curveLength, remaining);
+
+            if(prev_pos == Vector2.zero){
+                prev_pos = GetNewPos(startingPos, endingPos, normal, xOffset, yOffset);
+            }else{
+                Vector2 new_pos = GetNewPos(startingPos, endingPos, normal, xOffset, yOffset);
+                Gizmos.DrawLine(prev_pos, new_pos);
+                prev_pos = new_pos;
+            }
+            remaining -= step;
+        }
+    }
+
+    Vector2 GetNewPos(Vector2 startingPos, Vector2 endingPos, float normal, float xOffset = 0, float yOffset = 0){
+        float x = Mathf.Lerp(startingPos.x, endingPos.x, normal);
+        float y = (Mathf.Sin(Mathf.Lerp(0, Mathf.PI, normal)) * _jumpHeight) + Mathf.Lerp(startingPos.y, endingPos.y, normal);
+        return new Vector2(x + xOffset, y + yOffset);
     }
 }

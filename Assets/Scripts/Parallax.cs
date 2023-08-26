fc5ed6283381b612d1807c0eba9b979745f29ce1
G3ru1a <egidiufarcas@maze.ws>
2023-08-26 15:33:07 +0100

    added fall_bounds, made camera follow Y axis

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -19,7 +19,7 @@ public class Parallax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startPos + dist, cam.transform.position.y, transform.position.z);
 
         if (temp > startPos + lenght) startPos += lenght;
         else if (temp < startPos - lenght) startPos -= lenght;
